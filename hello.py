b872d9632d8dc11a901597f097275cb06119b1a6
Jon-Cody Sokoll <sokoll.jc@gmail.com>
2019-11-13 17:29:12 -0600

    Added say_bye function

--- hello.py
+++ hello.py
@@ -5,8 +5,12 @@ Prints Hello World
 def say_hi(name):
     return f'Hello {name}!'
 
+def say_bye(name):
+    return f'Bye {name}'
+
 
 if __name__ == 'main':
 
     say_hi("World!")
+    say_bye("World!")
 
